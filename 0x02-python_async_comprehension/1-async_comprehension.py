984fd486eeb7c95197a5d78238f9986bbf886587
amlaku <amlakubeyene@gmail.com>
2023-01-10 21:05:39 +0300

    async comprehetion

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
-"""
-Async Comprehensions
-Write coroutine async_comprehension, takes no arguments
-Return 10 random numbers collected using async comprehensions
-"""
+
+""" Async Comprehensions """
+
+from asyncio import sleep
+from random import uniform
 from typing import List
-Vector = List[float]
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Vector:
-    """
-    Coroutine async_comprehension
-    yield random numbers collected
-    """
-    stop = [y async for y in async_generator()]
-    return stop
+async def async_comprehension() -> List[float]:
+    """ Async Comprehensions  """
+    a = [i async for i in async_generator()]
+    return a
